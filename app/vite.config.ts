815800c3bef526681c452d7829f8abe0ae38de95
denso10911 <denso10911@gmail.com>
2025-03-07 22:12:52 +0200

    Configure client-side routing for Vite and Vercel.
    Added `historyApiFallback` to Vite's dev server for SPA routing. Updated Vercel's config to handle fallback routing to `index.html`.

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig(({ mode }) => {
   return {
     base: env.VITE_BASE_PATH,
     plugins: [react(), tsconfigPaths()],
+    server: {
+      historyApiFallback: true
+    },
     resolve: {
       alias: {
         "@": "/src",
